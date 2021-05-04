b6c34179e395c5efd4db1d6d99eaf4d464548c19
rishabhkapar <rishabhkapar.unofficial@gmail.com>
2021-05-05 00:57:16 +0530

    added dependencies

---
+++ Backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const server_config = require("./server_config");
+
+const app = express();
+
+app.listen(server_config.PORT, (err) => {
+  if (err)
+    return console.log("An error has occured while starting the server");
+  console.log("Server is up and running at http://localhost:" + server_config.PORT);
+})
